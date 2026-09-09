a6ac85b30a5d858f10b428d7e61321130808fe25
meg0302 <gupshreeya0504@gmail.com>
2026-09-09 18:52:50 +0530

    Force deployment v1.2.1 with cache busting

--- web3-integration.js
+++ web3-integration.js
@@ -403,5 +403,5 @@ if (typeof module !== 'undefined' && module.exports) {
   module.exports = Web3Manager;
 }
 
-console.log('🔗 Web3 Integration loaded - v1.2');
+console.log('🔗 Web3 Integration loaded - v1.2.1');
 console.log('💡 Call web3Manager.connect() to connect MetaMask');
